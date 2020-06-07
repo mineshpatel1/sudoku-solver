2d334a7fde02a9a9d56d969f08251be98901bef3
Christophe Lambin <christophe.lambin@gmail.com>
2020-06-07 14:01:25 +0200

    build w/ tensorflow 2.2 & opencv 4.2

--- neural_net/digits.py
+++ neural_net/digits.py
@@ -5,7 +5,8 @@ Based heavily on TensorFlow's Deep MNIST tutorial: https://www.tensorflow.org/ge
 
 import os
 import numpy as np
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 
 
 def weight_variable(shape):
